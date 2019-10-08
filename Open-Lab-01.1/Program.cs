50c0afd51eb8853037b9645cf85335494cb188c8
romankalab <roan.kalab@mail.com>
2019-10-08 22:08:54 +0200

    Initial

---
+++ Open-Lab-01.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Open_Lab_01._1
+{
+    class GFG
+    {
+        public static void Main()
+        {
+            int number;
+
+            Console.WriteLine("Enter your number: ");
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            {
+                if (number < 10)
+                    Console.WriteLine("The number is less than 10.");
+                else
+                    Console.WriteLine("This number is greater or equal to 10.");
+
+            }
+        }
+    }
+}
